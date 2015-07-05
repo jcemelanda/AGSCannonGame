400faf872d828109feee8145fad1d2267a8e3250
Carlos Leite <carlos.leite@znc.com.br>
2015-07-05 15:21:10 -0300

    fixed pep8

--- server.py
+++ server.py
@@ -4,9 +4,11 @@ from pickle import load, dump
 db_file = 'players.db'
 action_file = 'actions.db'
 
+
 def load_db():
     with open(db_file, 'rb') as db:
         return load(db)
+
 
 def dump_db(obj):
     with open(db_file, 'wb') as db:
@@ -17,10 +19,12 @@ try:
 except:
     players = {}
 
+
 def write_action(action):
     print(action)
     with(open(action_file, 'a')) as actions:
         actions.write(action)
+
 
 @route('/goto/', method='POST')
 def goto():
@@ -28,14 +32,17 @@ def goto():
   distance = request.POST.get('distance')
   write_action('goto:{}:{}:{}\n'.format(request.remote_addr, direction, distance))
 
+
 @route('/shoot/', method='POST')
 def shoot():
   direction = request.POST.get('direction')
   write_action('shoot:{}:{}\n'.format(request.remote_addr, direction))
 
+
 @route('/get_players/')
 def get_players():
     return players
+
 
 @route('/imalive/', method='POST')
 def im_alive():
@@ -43,6 +50,7 @@ def im_alive():
     players[request.remote_addr] = {'name': name}
     dump_db(players)
     write_action('add_player:{}:{}\n'.format(request.remote_addr, name))
+
 
 @route('/start_game/')
 def start_game():
